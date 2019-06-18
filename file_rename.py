2172fcdc329d748159434b7a104716c9e5f72ceb
Ryan Elliott <rselliott99@gmail.com>
2019-06-18 10:58:16 -0400

    Fix rename approach

--- file_rename.py
+++ file_rename.py
@@ -1,22 +1,25 @@
 import os
 
 def main():
-    i = 0
+    img_number = 0
     path = "C:\\Users\\Ryan\\Desktop\\test"
 
     print(path)
-    for file in os.listdir(path):
 
-        size = (os.path.getsize(path + "\\" + file)) / 1024 # Get the file size and convert it to KB
-        size = round(size, 2)
-        print("File: {}\nSize: {} KB".format(file, size))
+    for folder in os.listdir(path):
+        print(folder)
+        temp_path = path + "\\" + folder
+        for file in os.listdir(temp_path):
+            size = (os.path.getsize(temp_path + "\\" + file)) / 1024 # Get the file size and convert it to KB
+            size = round(size, 2)
+            print("File: {}\nSize: {} KB".format(file, size))
 
-        if size < 500:
-            loc1 = file.find("_")
-            loc2 = file.index(".jpg")
+            if size < 500:
+                img_number += 1
+                print(img_number)
 
-            print("Location 1: {}\tLocation 2: {}".format(loc1, loc2))
-
-            os.rename(file, file[loc1:loc2])
+                output_path = path + "\\Processed\\"
+                print(output_path)
+                os.rename(temp_path + "\\" + file, output_path + "Processed IMG_{}.JPG".format(img_number))
 
 main()
