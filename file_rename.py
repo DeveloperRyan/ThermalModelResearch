d4a741c9c359c602fc045ec602514827b72328b5
Ryan Elliott <rselliott99@gmail.com>
2019-06-18 11:27:59 -0400

    Add comments for code breakdown

--- file_rename.py
+++ file_rename.py
@@ -1,28 +1,28 @@
-import os
+# TODO
+# Add CLI or GUI to change path
 
-def main():
-    img_number = 0
-    path = "C:\\Users\\Ryan\\Desktop\\test"
+import os # Import the OS library
 
-    print(path)
+def main(): # Define the main function
+    path = "C:\\Users\\Ryan\\Desktop\\test" # Define the initial path of the non-processed images
 
-    for folder in os.listdir(path):
-        if folder == "Processed":
-            continue
-            
-        print(folder)
-        temp_path = path + "\\" + folder
-        for file in os.listdir(temp_path):
+    for folder in os.listdir(path): # Loop through each folder in path
+    if folder == "Processed": # If Processed is the name of the current folder
+            continue # Skip to the next loop
+
+        temp_path = path + "\\" + folder # Temporary path for each folder
+
+        img_number = 0 # Image counter for naming the images
+        for file in os.listdir(temp_path): # Loop through each file within each folder
             size = (os.path.getsize(temp_path + "\\" + file)) / 1024 # Get the file size and convert it to KB
-            size = round(size, 2)
-            print("File: {}\nSize: {} KB".format(file, size))
+            size = round(size, 2) # Round file size to 2 decimal places
+            print("File: {}\nSize: {} KB".format(file, size)) # Print each file & size
 
-            if size < 500:
-                img_number += 1
-                print(img_number)
+            if size < 500: # Checking files under 500 KB, all thermal images we are working with are under this size
+                img_number += 1 # Raise the number by one for each image
 
-                output_path = path + "\\Processed\\"
-                print(output_path)
-                os.rename(temp_path + "\\" + file, output_path + "Processed IMG_{}.JPG".format(img_number))
+                output_path = path + "\\Processed\\" # Change the output path to the processed folder
+                os.rename(temp_path + "\\" + file, output_path + "Processed IMG_{}.JPG".format(img_number)) # Rename and move all the
 
-main()
+
+main() # Run the main function
